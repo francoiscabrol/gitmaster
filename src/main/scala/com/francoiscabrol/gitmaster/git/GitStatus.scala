f50aec9e995000975949e3e679924cd0911ee5f1
Francois Cabrol <francois.cabrol@live.ca>
2016-11-04 16:58:27 -0700

    Fix a bug when the status is need to push

--- src/main/scala/com/francoiscabrol/gitmaster/git/GitStatus.scala
+++ src/main/scala/com/francoiscabrol/gitmaster/git/GitStatus.scala
@@ -3,9 +3,10 @@ package com.francoiscabrol.gitmaster.git
 import com.francoiscabrol.screen.Colored._
 
 object GitStatus extends Enumeration {
-  val NOT_SYNC, UP_TO_DATE, UNSTAGE_FILES, UNEXPECTED_ERROR, NO_REMOTE, CHANGES_TO_COMMIT = Value
+  val NOT_SYNC, UP_TO_DATE, UNSTAGE_FILES, UNEXPECTED_ERROR, NO_REMOTE, CHANGES_TO_COMMIT, CHANGES_TO_PUSH = Value
 
   def litteralStatus(status: Value) = status match {
+    case GitStatus.CHANGES_TO_PUSH => "Need to push".red
     case GitStatus.CHANGES_TO_COMMIT => "Need to commit".red
     case GitStatus.UNSTAGE_FILES => "Unstaged files".red
     case GitStatus.UP_TO_DATE => "Up to date".green
